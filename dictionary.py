a5f02c13d989187317371d15d487bdce98c23eda
RonMathew123 <ronmathew2007@gmail.com>
2024-08-20 23:06:58 +0530

    Create dictionary.py
    add file

---
+++ dictionary.py
@@ -0,0 +1,44 @@
+dic={"cat":"an animal","dog":"an animal"}
+def check():
+    d=0
+    word1=word.get()
+    a="The meaning of the word "
+    b=" is "
+    for i in dic:
+        if word1.lower()==i:
+            Resut=tk.Tk()
+            Resut.geometry('500x200')
+            Resut.title("Result found")
+            Input.destroy()
+            a+=word1
+            a+=b
+            a+=dic[i]
+            meaning=tk.Label(Resut,text=a)
+            meaning.pack()
+            end=tk.Button(Resut,text="Done",command=Resut.destroy)
+            end.pack()
+            Resut.mainloop()
+            d=1
+    if d==0:
+        Resut=tk.Tk()
+        Resut.geometry('500x200')
+        Resut.title("Result not found")
+        Input.destroy()
+        result= tk.Label(Resut,text="No word found")
+        result.pack()
+        Return=tk.Button(Resut,text="Try again",command=Resut.destroy)
+        Return.pack()
+import tkinter as tk
+
+Input=tk.Tk()
+Input.geometry('500x200')
+Input.title("Dictionary")
+lab=tk.Label(Input, text='Enter the word:')
+lab.pack()
+word=tk.Entry(Input, justify="left")
+word.pack()
+submit=tk.Button(Input,text="Submit",command=check,)
+submit.pack()
+Input.mainloop()
+
+
